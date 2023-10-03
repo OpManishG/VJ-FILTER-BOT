dadc6441b16b36d565cbf0cfeac9ea4c2cc1ea37
OpManishG <145533753+OpManishG@users.noreply.github.com>
2023-10-03 08:35:44 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -226,10 +226,9 @@ IMDb Data:
 📀 RunTime: {runtime} Minutes
 📆 Release Info : {release_date}
 🎛 Countries : <code>{countries}</code>
+
+
 ⏰Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
-
-
-👺 Join First: @TheSpeedMovies</b>"""
 
 Requested by : {message.from_user.mention}</b>"""
     
