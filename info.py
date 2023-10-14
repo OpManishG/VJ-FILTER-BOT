c3dbdb2f9d948cc53c17d052a64984ed999b5a02
OpManishG <145533753+OpManishG@users.noreply.github.com>
2023-10-14 17:03:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://opmanishg:manish123@cluster0.9zpx8h4.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
-DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
+DATABASE_NAME = environ.get('DATABASE_NAME', "opmanish")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
